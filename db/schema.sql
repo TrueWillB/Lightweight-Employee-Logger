3207ead32d2b7a89ac910873b7603bb7bb98b27f
William Brown <willis291@gmail.com>
2023-06-22 20:07:53 -0600

    Fixed Schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,27 +3,29 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE employee(
+CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  FOREIGN KEY role_id 
-  REFERENCES role(id)
-  ON DELETE SET NULL,
-  manager_id INT REFERENCES employee(id)
-  ON DELETE SET NULL,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
   salary INT NOT NULL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 
-CREATE TABLE department(
+CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  manager_id INT REFERENCES employee(id)
+  ON DELETE SET NULL
 );
